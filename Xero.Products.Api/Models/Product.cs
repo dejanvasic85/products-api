18067999699af14e4ee5ba5b9653878145c65891
Dejan Vasic <dejanvasic24@gmail.com>
2020-04-25 15:49:19 +1000

    Cleaning up the bad and horrible code

--- Xero.Products.Api/Models/Product.cs
+++ Xero.Products.Api/Models/Product.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using Newtonsoft.Json;
 
 namespace Xero.Products.Api.Models
 {
@@ -17,11 +15,8 @@ namespace Xero.Products.Api.Models
 
         }
 
-
-        [Required]
         public Guid Id { get; set; }
 
-        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
